701cc84d87ae523f0f5aa818d63ddfd3a33bf84c
刘放 <362512489@qq.com>
2019-04-12 13:16:01 +0800

    style: change demo name

--- vueCliDemo/vueCliBase/basic/src/views/Home.vue
+++ vueCliDemo/vueCliBase/basic/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App,this is brizer app , hehe"/>
   </div>
 </template>
 
