58df3180bb0936a7f910311cf879ea276b41c54c
刘放 <362512489@qq.com>
2021-05-12 14:27:54 +0800

    faet: snipptes

---
+++ snippets/scripts.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+        beforeCreate () {
+            ;
+        },
+        created () {
+            ;
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ snippets/templates.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+        <input v-model="data" type="text" />
+        <div @click="handler(arg, event)"></div>
+        <img :src="'/path/to/images/' + fileName" alt="altText"/>
+
+        <div :class="{ className: data }"></div>
+
+        <div :class="[classNameA, classNameB]"></div>
+
+        <div :class="[classNameA, {classNameB : condition}]"></div>
+
+        <transition
+            mode="out-in"
+            @before-enter="beforeEnter"
+            @enter="enter"
+        
+            @before-leave="beforeLeave"
+            @leave="leave"
+            :css="false">
+        
+        </transition>
+
+        <router-link :to="{name: 'name'}">LinkTitle</router-link>
+
+        <router-link :to="{name: 'name', params:{id: 'value'} }">LinkTitle</router-link>
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
